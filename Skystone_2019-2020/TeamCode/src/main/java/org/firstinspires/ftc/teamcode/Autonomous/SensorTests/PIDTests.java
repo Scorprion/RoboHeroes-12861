9d24fbb86b71d5f0f133151c2106cb9e424f3cc8
Dylan <dylanchoi1@gmail.com>
2019-10-30 17:39:50 -0700

    Vuforia unfinished
    The movement still has issues, particularly the straight PID movement for some reason. Good news though: the Vuforia detection works

---
+++ Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SensorTests/PIDTests.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Autonomous.SensorTests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Init.Aggregated;
+
+@Autonomous(name = "PID Test", group = "Autonomous")
+public class Auto_A extends Aggregated {
+}
