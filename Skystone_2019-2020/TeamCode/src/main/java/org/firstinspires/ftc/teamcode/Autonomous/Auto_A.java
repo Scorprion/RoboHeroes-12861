e29ebd4cabf6279616191fb3707a534cc541ce48
Dylan <dylanchoi1@gmail.com>
2019-11-01 17:47:34 -0700

    Fixed, but not
    After the encoders broke (because its the day before meet 0 of course), we figured out the Arm encoder messed up the Left and Right motor encoders. Unplugging it, it fixed the issue of the Left and Right encoders not working, but in turn messed up all of our A-D programs.

--- Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_A.java
+++ Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_A.java
@@ -31,7 +31,6 @@ public class Auto_A extends Aggregated {
     private double speed = 0.1, pidOutput = 0;
     private PID pid = new PID(0.5, 0.5, 0, 0);
     private VectorF locationV;
-    private ElapsedTime timer = new ElapsedTime();
     public boolean VuforiaFound = false;
 
     @Override
@@ -41,69 +40,25 @@ public class Auto_A extends Aggregated {
 
         robot.Arm.setPower(0.5);
 
-        timer.reset();
         encoderDrives(0.3, 22, 22, pid);
-        pid.setParams(1.45, 0.5, 0, 90);
-        timer.reset();
-        while (timer.milliseconds() < 5500 && opModeIsActive()) {
-            telemetry.addData("Angle: ", robot.imu.getAngularOrientation().firstAngle);
-            telemetry.update();
-            pidOutput = pid.getPID(robot.imu.getAngularOrientation().firstAngle);
-            robot.Left.setPower(-pidOutput);
-            robot.Right.setPower(pidOutput);
-        }
-        timer.reset();
+
+        pid.setParams(1.45, 0.5, 0, 270);
+        run_pid(0, 5500, pid, true);
+
         encoderDrives(0.3, 18,18);
+
         pid.setParams(1.52, 0.5, 0, 0);
-        timer.reset();
-        while (timer.milliseconds() < 5500 && opModeIsActive()) {
-            telemetry.addData("Angle: ", robot.imu.getAngularOrientation().firstAngle);
-            telemetry.update();
-            pidOutput = pid.getPID(robot.imu.getAngularOrientation().firstAngle);
-            robot.Left.setPower(-pidOutput);
-            robot.Right.setPower(pidOutput);
-        }
+        run_pid(0, 5500, pid, true);
+
         encoderDrives(0.3, 10, 10);
         robot.Arm.setPower(0);
         sleep(1000);
         encoderDrives(0.3, -10, -10);
-        encoderDrives(0.2, -10, 10);
-        encoderDrives(0.7, 60,60);
-
-
-
-
-        /*pid.setParams(0.5, 0.5, 0, 90);
-        while (timer.milliseconds() < 5000 && opModeIsActive()) {
-            telemetry.addData("Angle: ", robot.imu.getAngularOrientation().firstAngle);
-            telemetry.update();
-            pidOutput = pid.getPID(robot.imu.getAngularOrientation().firstAngle);
-
-            robot.Left.setPower((pidOutput* 1.6) + 0.3);
-            robot.Right.setPower(0.3);
-            vuforia();
-        } */
-
-
-
-
-
-
-
-
-
-        /*
-        while (opModeIsActive()) {
-            locationV = vuforia();
-            if (locationV != null) {
-                pid.getPID(locationV.get(2));
-                robot.Left.setPower(speed + pid.PIDout);
-                robot.Right.setPower(speed + pid.PIDout);
-            } else {
-                robot.Left.setPower(0);
-                robot.Right.setPower(0);
-            }
-    }
-*/
+        encoderDrives(0.2, -13, 13);
+        encoderDrives(0.7, 103,103);
+        robot.Arm.setPower(1);
+        sleep(1000);
+        robot.Arm.setPower(0);
+        encoderDrives(0.5, -60, -60);
     }
 }
