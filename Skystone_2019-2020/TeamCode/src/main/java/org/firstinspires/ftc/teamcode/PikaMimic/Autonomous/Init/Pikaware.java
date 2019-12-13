502855120b63f756fea08130c7895ab2d266420a
Wowwer <brickknightcollin@gmail.com>
2019-12-13 15:50:14 -0800

    Pikabot created

--- Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PikaMimic/Autonomous/Init/Pikaware.java
+++ Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PikaMimic/Autonomous/Init/Pikaware.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.PikaMimic.Autonomous.Init;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Pikaware {
-    public Left DcMotor;
-    public Right DcMotor;
+    public DcMotor Left;
+    public DcMotor Right;
 
     public void init(HardwareMap ahwMap){
-        Right = ahwMap.get(DcMotor.class, "Right");
+        Right = ahwMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "Right");
         Right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Right.setDirection(DcMotorSimple.Direction.REVERSE);
 
