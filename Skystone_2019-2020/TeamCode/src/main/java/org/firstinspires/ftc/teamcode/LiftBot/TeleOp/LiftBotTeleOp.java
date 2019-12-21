d705019674a25358453d05e2e0389e17ee05b2cd
Wowwer <brickknightcollin@gmail.com>
2019-12-21 10:26:36 -0800

    Merge fix 2

--- Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftBot/TeleOp/LiftBotTeleOp.java
+++ Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftBot/TeleOp/LiftBotTeleOp.java
@@ -10,40 +10,41 @@ import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 @TeleOp(name= "TeleOp LiftBot", group= "Pushbot")
 public class LiftBotTeleOp extends OpMode {
 
-        CRServo Left;
-        CRServo Right;
-        CRServo Grip1;
-        CRServo Grip2;
-        CRServo TurnGrip;
+    CRServo Left;
+    CRServo Right;
+    CRServo Grip1;
+    CRServo Grip2;
+    CRServo TurnGrip;
 
-        public void init(){
-            Left = hardwareMap.get(CRServo.class, "Left");
-            Right = hardwareMap.get(CRServo.class, "Right");
-            Grip1 = hardwareMap.get(CRServo.class, "Grip1");
-            Grip2 = hardwareMap.get(CRServo.class, "Grip2");
-            Grip2.setDirection(REVERSE);
-            TurnGrip = hardwareMap.get(CRServo.class, "TurnGrip");
-        }
+    public void init() {
+        Left = hardwareMap.get(CRServo.class, "Left");
+        Right = hardwareMap.get(CRServo.class, "Right");
+        Grip1 = hardwareMap.get(CRServo.class, "Grip1");
+        Grip2 = hardwareMap.get(CRServo.class, "Grip2");
+        Grip2.setDirection(REVERSE);
+        TurnGrip = hardwareMap.get(CRServo.class, "TurnGrip");
+    }
 
-        public void loop(){
+    public void loop() {
 
         if (gamepad1.left_stick_y >= 0 || gamepad1.left_stick_y <= 0) {
             double upDown = gamepad1.left_stick_y;
 
-                if (gamepad1.left_stick_y <= -0.1 || gamepad1.left_stick_y >= 0.1){
-                    Left.setPower(upDown);
-                    Right.setPower(-upDown);
-                }else {
-                    Left.setPower(0);
-                    Right.setPower(0);
-                }
+            if (gamepad1.left_stick_y <= -0.1 || gamepad1.left_stick_y >= 0.1) {
+                Left.setPower(upDown);
+                Right.setPower(-upDown);
+            } else {
+                Left.setPower(0);
+                Right.setPower(0);
+            }
 
-        if (gamepad1.a){
-                    Grip1.setPower(1);
-                    Grip2.setPower(1);
-                }else{
-                    Grip1.setPower(0);
-                    Grip1.setPower(0);
-                }
+            if (gamepad1.a) {
+                Grip1.setPower(1);
+                Grip2.setPower(1);
+            } else {
+                Grip1.setPower(0);
+                Grip1.setPower(0);
             }
         }
+    }
+}
