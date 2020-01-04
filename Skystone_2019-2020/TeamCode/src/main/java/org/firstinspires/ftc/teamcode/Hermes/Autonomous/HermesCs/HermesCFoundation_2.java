b5745cebf9e5d87f10d00d62cd1aef528ceda0a2
Wowwer <brickknightcollin@gmail.com>
2020-01-04 13:43:34 -0800

    Finished Fine Tuning for B and C
    Code B and C are finished. Need to upload A and D codes.

--- Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hermes/Autonomous/HermesCs/HermesCFoundation_2.java
+++ Skystone_2019-2020/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hermes/Autonomous/HermesCs/HermesCFoundation_2.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Hermes.Autonomous.HeremesBs;
+package org.firstinspires.ftc.teamcode.Hermes.Autonomous.HermesCs;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -7,8 +7,9 @@ import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.teamcode.Hermes.Autonomous.Init.HermesAggregated;
 import org.firstinspires.ftc.teamcode.PID;
 
-@Autonomous(name = "HermesBFoundation", group = "Hermes")
-public class HermesBFoundation extends HermesAggregated {
+@Autonomous(name = "HermesCFoundation_2", group = "Hermes")
+
+public class HermesCFoundation_2 extends HermesAggregated {
 
     public boolean VuforiaFound = false;
 
@@ -17,7 +18,7 @@ public class HermesBFoundation extends HermesAggregated {
         robot.init(hardwareMap);
         waitForStart();
         encoderDrives(0.5, 24, 24, 1.5);
-        mecanumMove(0.5, 90, 12, 1.5);
+        mecanumMove(-0.5, 90, 12, 1.5);
         encoderDrives(0.25, 10, 10, 2.5);
 
         robot.FoundationClaw.setPower(1);
@@ -25,18 +26,18 @@ public class HermesBFoundation extends HermesAggregated {
         robot.FoundationClaw.setPower(0);
         sleep(1000);
 
-        mecanumMove(-0.5, 90, 12, 1.7);
+        //mecanumMove(0.5, 90, 12, 1.7);
         encoderDrives(0.5, -15, -15, 1.5);
-        pidTurn(2.5, 0.2, 0.1, -90, 0, 3.0);
+        pidTurn(2.5, 0.2, 0.1, 90, 0, 3.0);
         //encoderDrives(0.4, 45, -45, 2.5); // Turning 90 degrees
-        encoderDrives(0.4, 20, 24, 1.5);
+        encoderDrives(0.4, 24, 20, 1.5);
 
         robot.FoundationClaw.setPower(-1);
         sleep(1000);
         robot.FoundationClaw.setPower(0);
-        mecanumMove(0.5, 90, 12, 1.5);
-        encoderDrives(0.5, -47, -47, 5);
-        /*mecanumMove(0.5, 90, 10, 1.5);
-        encoderDrives(0.4, -24, -24, 1.5);*/
+        mecanumMove(-0.5, 90, 35, 3);
+        sleep(10000);
+        encoderDrives(0.5, -40, -40, 5);
+
     }
 }
