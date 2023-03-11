81bfb3c620891efae37148a574344f639c22ddf8
scorprion <dylanchoi1@gmail.com>
2023-03-10 18:03:06 -0800

    Disabled deposit code (old)

--- 2022_2023_Thor/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/auto/ThorBlueRightDeposit.java
+++ 2022_2023_Thor/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/auto/ThorBlueRightDeposit.java
@@ -26,6 +26,7 @@ import android.annotation.SuppressLint;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -39,6 +40,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import java.util.ArrayList;
 
 @Autonomous
+@Disabled
 public class ThorBlueRightDeposit extends LinearOpMode
 {
    AprilTagDetectionPipeline aprilTagDetectionPipeline;
